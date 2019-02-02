f3932ef9efd204c3649a03c2fe8a4b8d5111001a
vladimirpikul <vladimir.pikul.1985@gmail.com>
2019-02-02 14:40:01 +0200

    adaptive ready

--- src/js/modules/services-slider.js
+++ src/js/modules/services-slider.js
@@ -1,12 +1,19 @@
 const servicesSlider = () => {
-  const mySwiper = new Swiper('.services-swiper', {
-    loop: true,
-    slidesPerView: 4,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  if ($(window).width() > 980) {
+    const mySwiper = new Swiper('.services-swiper', {
+      loop: true,
+      slidesPerView: 4,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } else {
+    //console.log(mySwiper);
+    $('.services-swiper .swiper-wrapper').removeAttr('style');
+    $('.services-swiper .swiper-slide').removeAttr('style');
+  }
 };
+
 
 export default servicesSlider;
